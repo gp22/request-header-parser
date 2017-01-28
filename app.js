aa277c2e573b1a6ab0f5c89d67addb95393e87f2
gp22 <gp22@tutanota.com>
2017-01-28 10:34:33 +0000

    use req.headers[x-forwarded-for] for correct ip

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
     var response = {};
 
-    // var ip = req.ip;
     var ip = req.headers['x-forwarded-for'];
-    // response["ipaddress"] = ip.slice(ip.lastIndexOf(':')+1);
     response["ipaddress"] = ip;
 
     var os = req.headers['user-agent'];
